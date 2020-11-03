8e0b087055b3997fac6c0cb37eb4bbfe2fcc5e28
swissglider <npm@mailschweiz.com>
2020-11-03 13:16:18 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'area-floor-room-handler adapter settings': {
+        en: 'Adapter settings for area-floor-room-handler',
+        de: 'Adaptereinstellungen für area-floor-room-handler',
+        ru: 'Настройки адаптера для area-floor-room-handler',
+        pt: 'Configurações do adaptador para area-floor-room-handler',
+        nl: 'Adapterinstellingen voor area-floor-room-handler',
+        fr: "Paramètres d'adaptateur pour area-floor-room-handler",
+        it: "Impostazioni dell'adattatore per area-floor-room-handler",
+        es: 'Ajustes del adaptador para area-floor-room-handler',
+        pl: 'Ustawienia adaptera dla area-floor-room-handler',
+        'zh-cn': 'area-floor-room-handler的适配器设置',
+    },
+    option1: {
+        en: 'option1',
+        de: 'option1',
+        ru: 'option1',
+        pt: 'option1',
+        nl: 'option1',
+        fr: 'option1',
+        it: 'option1',
+        es: 'option1',
+        pl: 'option1',
+        'zh-cn': 'option1',
+    },
+    option2: {
+        en: 'option2',
+        de: 'option2',
+        ru: 'option2',
+        pt: 'option2',
+        nl: 'option2',
+        fr: 'option2',
+        it: 'option2',
+        es: 'option2',
+        pl: 'option2',
+        'zh-cn': 'option2',
+    },
+};
